60f68c2345fa2dfb77add2ed5a67b70838014269
SimpleSmart <henrick.muis@gmail.com>
2019-02-25 03:11:41 +0100

    make member functions static on library class

--- apiclientlibrary/src/main/java/logos/com/apiclientlibrary/PrintUtil.java
+++ apiclientlibrary/src/main/java/logos/com/apiclientlibrary/PrintUtil.java
@@ -3,11 +3,11 @@ package logos.com.apiclientlibrary;
 import android.util.Log;
 
 public class PrintUtil {
-    public void printError(String tag, int number) {
+    static public void printError(String tag, int number) {
         Log.e(tag, number + "");
     }
 
-    public void printError(String tag, Integer number) {
+    static public void printError(String tag, Integer number) {
         Log.e(tag, number + "");
     }
 }
--- app/src/main/java/logos/com/gradlelibrary/MainActivity.java
+++ app/src/main/java/logos/com/gradlelibrary/MainActivity.java
@@ -3,11 +3,15 @@ package logos.com.gradlelibrary;
 import android.app.Activity;
 import android.os.Bundle;
 
+import logos.com.apiclientlibrary.PrintUtil;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PrintUtil.printError("LibraryProject", 1);
     }
 }
