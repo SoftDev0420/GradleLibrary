717095226c4815945a5ff2244fca4e6e8c4392b7
Admin <admin@Admins-iMac-2.local>
2020-01-15 04:38:39 +0100

    add classes to integrate cosential api

--- app/src/main/java/logos/com/gradlelibrary/MainActivity.java
+++ app/src/main/java/logos/com/gradlelibrary/MainActivity.java
@@ -2,8 +2,18 @@ package logos.com.gradlelibrary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-import logos.com.apiclientlibrary.PrintUtil;
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import logos.com.apiclientlibrary.WebService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +22,65 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PrintUtil.printError("LibraryProject", 1);
+        final Gson gson = new Gson();
+
+        Call<List<Object>> signInCall = WebService.getInstance().signIn();
+        signInCall.enqueue(new Callback<List<Object>>() {
+            @Override
+            public void onResponse(Call<List<Object>> signInCall, Response<List<Object>> response) {
+                if (response != null) {
+                    if (response.body() == null) {
+                        Log.d("Gradle Library", "Unauthorized.");
+                    }
+                    else {
+                        Object currentUser = response.body().get(0);
+                        User user = gson.fromJson(gson.toJson(currentUser), User.class);
+                        Log.d("Gradle Library", user.firstName);
+                    }
+                }
+                else {
+                    Log.d("Gradle Library", "Incorrect response from server.");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Object>> signInCall, Throwable t) {
+                Log.d("Gradle Library", "Connection Error.");
+            }
+        });
+
+        List<Object> companies = new ArrayList<>();
+        Company company = new Company();
+        company.setName("Company Lib 1");
+
+        companies.add(Object.class.cast(company));
+
+
+        Call<List<Object>> addCompanyCall = WebService.getInstance().addCompanies(companies);
+
+        addCompanyCall.enqueue(new Callback<List<Object>>() {
+            @Override
+            public void onResponse(Call<List<Object>> addCompanyCall, Response<List<Object>> response) {
+                if (response != null) {
+                    if (response.body() == null || response.body().size() == 0) {
+                        Log.d("Gradle Library", "Error while adding a new company.");
+                    }
+                    else {
+                        List<Object> objectList = response.body();
+                        String objectsString = gson.toJson(objectList);
+                        List<Company> companyList = Arrays.asList(gson.fromJson(objectsString, Company[].class));
+                        Company addedCompany = companyList.get(0);
+                    }
+                }
+                else {
+                    Log.d("Gradle Library", "Error while adding a new company.");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Object>> addCompanyCall, Throwable t) {
+                Log.d("Gradle Library", "Error while adding a new company.");
+            }
+        });
     }
 }
---
+++ app/src/main/java/logos/com/gradlelibrary/User.java
@@ -0,0 +1,24 @@
+package logos.com.gradlelibrary;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    @SerializedName("UserId")
+    @Expose
+    public int userId;
+    public String username;
+    public String password;
+    public String firmId;
+
+    @SerializedName("FirstName")
+    @Expose
+    public String firstName;
+
+    @SerializedName("LastName")
+    @Expose
+    public String lastName;
+}
